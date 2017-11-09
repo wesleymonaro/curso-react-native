bc8fc659364e6af0046ee47f807bdd5d2575f602
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-09 16:54:54 -0200

    Frases do dia - Criando App

---
+++ FrasesDoDia/index.android.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, AppRegistry} from 'react-native'
+
+const App = () => {
+  return(
+    <Text>Olá</Text>
+  );
+}; 
+
+AppRegistry.registerComponent('FrasesDoDia', () => App)
